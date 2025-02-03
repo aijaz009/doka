a1063f84fedbf4d036a3ef08cca74b7e0505414b
aijaz009 <38639256+aijaz009@users.noreply.github.com>
2025-02-03 12:18:32 +0530

    Update form.js

--- form.js
+++ form.js
@@ -101,8 +101,7 @@ class FormHandler {
         
         // Title
         doc.setFontSize(16);
-        doc.text("POLICE STATION LAR", 105, 15, { align: "center" });
-        doc.text("DETAILS OF NOMADS", 105, 15, { align: "center" });
+        doc.text("POLICE STATION LAR DETAILS OF NOMADS", 105, 15, { align: "center" });
         
         // Add photo if exists
         const photoData = cameraHandler.getImageData();
